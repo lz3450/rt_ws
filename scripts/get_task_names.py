e51948e9705ff364248c06bc215910a8e5b8cc24
RoboTrace <rt@s3lab.org>
2025-06-24 15:31:38 -0500

    scripts: add get_task_names.py

---
+++ scripts/get_task_names.py
@@ -0,0 +1,13 @@
+PLATFORMS=["sim", "phy"]
+ROBOT_TYPES=["wheeled", "arm"]
+ROBOT_TASKS={
+    "wheeled": ["navigating", "docking", "following"],
+    "arm": ["picking", "placing", "drawing"]
+}
+ATTACK_VECTORS=["config", "rogue_node", "algorithm", "actuator"]
+
+for platform in PLATFORMS:
+    for robot_type in ROBOT_TYPES:
+        for task in ROBOT_TASKS[robot_type]:
+            for attack_vector in ATTACK_VECTORS:
+                print(f"{platform}-{robot_type}-{task}-{attack_vector}")
