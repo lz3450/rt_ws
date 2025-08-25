0ab909727c9e90d4113b901a970362292cd19713
KZL <lz3450@outlook.com>
2025-08-25 07:08:45 -0500

    root dir scripts

--- rg_setup.sh
+++ rg_setup.sh
@@ -5,51 +5,28 @@ elif [ -n "$ZSH_VERSION" ]; then
     export RG_WS="$(cd -- "$(dirname "${(%):-%x}")" > /dev/null 2>&1; pwd -P)"
     shell="zsh"
 else
-  echo "Unsupported shell"
+    echo "Unsupported shell"
 fi
 echo "RG_WS=$RG_WS"
 
-echo "shell: $shell"
-ROS2_WS="$RG_WS/../../ros2_ws"
+################
+. "$RG_WS/../../ros2_ws/ros2_setup.sh"
+. "$RG_WS/../../ros2_ws/nav2_setup.sh"
+. "$RG_WS/../../ros2_ws/moveit2_setup.sh"
 
-ROS2_SETUP="$ROS2_WS/ros2_humble/install/local_setup.$shell"
-if [[ -f "$ROS2_SETUP" ]]; then
-    echo "ros2 humble ($shell)"
-    . "$ROS2_SETUP"
-else
-    echo "failed to set up ros2 humble"
-    exit 1
-fi
+################
+. "$RG_WS/../../IsaacSim-ros_workspaces/setup.sh"
 
-MOVEIT2_SETUP="$ROS2_WS/moveit2_humble/install/local_setup.$shell"
-if [[ -f "$MOVEIT2_SETUP" ]]; then
-    echo "moveit2 ($shell)"
-    . "$MOVEIT2_SETUP"
-else
-    echo "failed to set up moveit2"
-    exit 1
-fi
-
-NAV2_SETUP="$ROS2_WS/nav2_humble/install/local_setup.$shell"
-if [[ -f "$NAV2_SETUP" ]]; then
-    echo "nav2 ($shell)"
-    . "$NAV2_SETUP"
-else
-    echo "failed to set up nav2"
-    exit 1
-fi
-
+################
 RG_SETUP="$RG_WS/install/local_setup.$shell"
 if [[ -f "$RG_SETUP" ]]; then
     echo "rg_ws ($shell)"
     . "$RG_SETUP"
-
-    if [[ ! -f "$HOME/.ros/fastdds.xml" ]]; then
-        echo "Copying fastdds.xml to \"$HOME/.ros/\" ..."
-        mkdir -p "$HOME/.ros"
-        cp -v "$RG_WS/fastdds.xml" "$HOME/.ros/fastdds.xml"
-    fi
+else
+    echo "failed to set up rg_ws"
+    exit 1
 fi
 
+################
 export WAYLAND_DISPLAY="wayland-0"
 export DISPLAY=":0"
